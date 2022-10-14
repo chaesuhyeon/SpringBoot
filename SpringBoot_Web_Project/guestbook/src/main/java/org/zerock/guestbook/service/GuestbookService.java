75ba4db0d3eb60564c317d970fea85c2d79ff5f1
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-10-14 22:42:04 +0900

    목록페이지 처리

---
+++ SpringBoot_Web_Project/guestbook/src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -0,0 +1,38 @@
+package org.zerock.guestbook.service;
+
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
+
+public interface GuestbookService {
+
+    Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+    
+    // default 를 쓰면 impl에서  @Override 안하고 사용가능
+    default Guestbook dtoToEntity(GuestbookDTO dto){ // 서비스 계층에서는 파라미터를 DTO 타입으로 받기 때문에 이를 JPA로 처리하기 위해서는 엔티티 타입의 객체로 변환해야 하는 작업이 반드시 필요함
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity){
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+
+
+}
--- SpringBoot_Web_Project/guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ SpringBoot_Web_Project/guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -42,5 +49,58 @@ public class GuestbookRepositoryTests {
 
             guestbookRepository.save(guestbook);
         }
+    }
+
+    @Test
+    public void testQuery1(){ // 페이지 처리와 동시에 검색 처리가 가능
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; // 동적으로 처리하기 위해서 Q도메인 클래스를 얻어옴 , Q도메인 클래스를 이용하면 엔티티 클래스에 선언된 title, content같은 필드들을 변수로 활용할 수 있음
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder(); // BoardBuilder는 where문에 들어가는 조건들을 넣어주는 컨테이너라고 간주하면 됨
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword); // 원하는 조건은 필드값과 같이 결합해서 생성. BooleanBuilder안에 들어가는 값은 com.querydsl.core.types.Predicate 타입이어야함
+
+        builder.and(expression); // 만들어진 조건은 where문에 and나 or같은 키워드와 결합시킴
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
+    @Test
+    public void testQuery2(){
+        Pageable pageable = PageRequest.of(0,10,Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword="1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+
+        BooleanExpression exAll = exTitle.or(exContent);
+
+        builder.and(exAll);
+
+        builder.and(qGuestbook.gno.gt(0L));
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder,pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+
+
+
     }
 }
