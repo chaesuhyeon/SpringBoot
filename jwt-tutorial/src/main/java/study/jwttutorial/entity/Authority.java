41858333853c42a97ae9ca11e0c64d54d7684f79
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-04-03 19:14:53 +0900

    feat:  create sample entity

---
+++ jwt-tutorial/src/main/java/study/jwttutorial/entity/Authority.java
@@ -0,0 +1,20 @@
+package study.jwttutorial.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
