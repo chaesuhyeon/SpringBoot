3ad57e45546de7121be76d067eb7f57404dd58cf
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-04-11 13:01:54 +0900

    faet: 시큐리티를 이용한 권한 처리 완료

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +32,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                 .logoutSuccessUrl("/")
         ;
+
+        http.authorizeRequests() // 시큐리티 처리에 HttpServletRequest를 이용한다는 설정
+                .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll() // 모든 사용자가 인증 없이 해당 경로에 접근할 수 있도록 설정
+                .mvcMatchers("/admin/**").hasRole("ADMIN") // /admin으로 시작하는 경로는 해당 계정이 ADMIN일 때만 접근 가능하도록 설정
+                .anyRequest().authenticated(); // 위에서 설정한 경로를 제외한 나머지 경로들은 모두 인증을 요구하도록 설정
+        
+        http.exceptionHandling() 
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint()); // 인증되지 않은 사용자가 리소스에 접근하였을 때 수행되는 핸들러 설정
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**"); // static 디렉토리 하위 파일은 인증을 무시하도록 설정
     }
 
     @Override
