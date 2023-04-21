9e129de8f048080a247c32d56d8ca9d2c521b143
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-04-21 18:37:50 +0900

    feat: 1. custom exception생성 2.util 파일 생성 3.service 작성

--- jwt-tutorial/src/main/java/study/jwttutorial/controller/AuthController.java
+++ jwt-tutorial/src/main/java/study/jwttutorial/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.jwttutorial.dto.LoginDto;
 import study.jwttutorial.dto.TokenDto;
@@ -18,6 +19,7 @@ import study.jwttutorial.jwt.TokenProvider;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/api")
 public class AuthController {
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
