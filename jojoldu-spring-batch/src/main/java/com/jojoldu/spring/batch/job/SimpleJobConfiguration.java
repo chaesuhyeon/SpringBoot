32e7a6c77aa69d9d92917e2c9d3d5eb7db8b7a51
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-05-04 23:23:46 +0900

    simple job & simple step

---
+++ jojoldu-spring-batch/src/main/java/com/jojoldu/spring/batch/job/SimpleJobConfiguration.java
@@ -0,0 +1,36 @@
+package com.jojoldu.spring.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Configuration // Spring Batch의 모든 Job은 @Configuration으로 등록해서 사용
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() { // Job은 하나의 배치 작업 단위, Job안에는 여러개의 Step이 존재하고, Step안에 Tasklet 혹은 Reader & Processor & Writer 묶음이 존재
+        return jobBuilderFactory.get("simpleJob") // simpleJob이라는 이름으로 Batch Job을 생성
+                .start(simpleStep1())
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1") // simpleStep1이라는 이름으로 Batch Step을 생성
+                .tasklet(((contribution, chunkContext) -> { // Step안에서 수행될 기능들을 명시 , tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용
+                    log.info(">>>>> This is Step1"); // Batch가 수행되면 로그가 출력 되도록 함
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+}
---
+++ jojoldu-spring-batch/src/test/java/com/jojoldu/spring/batch/JojolduSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojolduSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
