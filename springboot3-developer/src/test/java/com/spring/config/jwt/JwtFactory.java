465ed8c73161495a272a471e65c53f07d105bcc0
chaesuhyeon <ddd8177@gmail.com>
2023-06-09 13:56:25 +0900

    tokenProvider 작성 후 테스트

---
+++ springboot3-developer/src/test/java/com/spring/config/jwt/JwtFactory.java
@@ -0,0 +1,45 @@
+package com.spring.config.jwt;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+public class JwtFactory {
+    private String subject = "test@test.com";
+    private Date issueAt = new Date();
+    private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
+    private Map<String, Object> claims = Collections.emptyMap();
+
+    @Builder
+    public JwtFactory(String subject, Date issueAt, Date expiration, Map<String, Object> claims) {
+        this.subject = subject != null ? subject : this.subject;
+        this.issueAt = issueAt != null ? issueAt : this.issueAt;
+        this.expiration = expiration != null ? expiration : this.expiration;
+        this.claims = claims != null ? claims : this.claims;
+    }
+
+    public static JwtFactory withDefaultValues() {
+        return JwtFactory.builder().build();
+    }
+
+    // jjwt 라이브러리를 사용해서 JWT 토큰 생성
+    public String createToken(JwtProperties jwtProperties){
+        return Jwts.builder()
+                .setSubject(subject)
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(issueAt)
+                .setExpiration(expiration)
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret_key())
+                .compact();
+    }
+}
