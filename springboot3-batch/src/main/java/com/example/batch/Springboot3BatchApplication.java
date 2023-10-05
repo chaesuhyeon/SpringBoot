9601b7487adbc61094e40d0c0259ef23b18a3757
chaesuhyeon <ddd8177@gmail.com>
2023-10-05 16:18:47 +0900

    batch version 5 simple practice

---
+++ springboot3-batch/src/main/java/com/example/batch/Springboot3BatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3BatchApplication.class, args);
+	}
+
+}
