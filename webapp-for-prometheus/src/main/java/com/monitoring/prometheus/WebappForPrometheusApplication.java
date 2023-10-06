bc147bd38301f941cad64ec380433f8c9dcdeff8
chaesuhyeon <ddd8177@gmail.com>
2023-10-06 23:06:36 +0900

    프로메테우스 연동 간단 예제

---
+++ webapp-for-prometheus/src/main/java/com/monitoring/prometheus/WebappForPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappForPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappForPrometheusApplication.class, args);
+	}
+
+}
