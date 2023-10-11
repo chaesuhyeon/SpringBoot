9d1e3bdcef9ac3d19cd07e3b61e364bfef1b39d6
chaesuhyeon <ddd8177@gmail.com>
2023-10-12 07:44:03 +0900

    core에 작성한 엔티티를 하위 모듈에서 사용가능 테스트 완료

---
+++ multi-module/example-core/src/main/java/com/example/multimodule/entity/Member.java
@@ -0,0 +1,13 @@
+package com.example.multimodule.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+    String name;
+    int age;
+}
