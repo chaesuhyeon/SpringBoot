fedbaa9a2029b2619812aa8e60f281c673c49883
chaesuhyeon <ddd8177@gmail.com>
2024-01-02 12:52:30 +0900

    [테스트를 추가하고 싶은 개발자들의 오답노트] > 간단한 사칙연산 로직과 단위테스트

---
+++ test-sample/src/main/java/com/example/test/Sample.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers and an oprator (e.g. 1 2 +): ");
+
+        String result = scanner.nextLine();
+        String[] parts = result.split(" ");
+        long num1 = Long.parseLong(parts[0]);
+        long num2 = Long.parseLong(parts[2]);
+        String operator = parts[1];
+
+        long answer = new Calculator().calculate(num1, operator, num2);
+
+        System.out.println(answer);
+    }
+}
