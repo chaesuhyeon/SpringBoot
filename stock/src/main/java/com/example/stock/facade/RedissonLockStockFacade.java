519bd5439a435baca3ecb9b2345995276422c7ff
suhyeon chae <ddd8177@gmail.com>
2024-01-14 20:32:35 +0900

    재고시스템으로 알아보는 동시성 문제 > redis redisson을 사용하여 동시성 문제 해결하기

---
+++ stock/src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,34 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+        try {
+            boolean available = lock.tryLock(15, 1, TimeUnit.SECONDS);
+            stockService.decrease(id, quantity);
+
+            if(!available) {
+                System.out.println("Lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
