e978b234e7d5161e6072ef44b8374ef6c60d5c57
chaesuhyeon <ddd8177@gmail.com>
2024-02-22 07:52:47 +0900

    커버리지를 위한 PostUpdate 테스트코드 작성

--- test-code-with-architecture-main/src/test/java/com/example/demo/post/domain/PostTest.java
+++ test-code-with-architecture-main/src/test/java/com/example/demo/post/domain/PostTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PostTest {
 
     @Test
-    public void PostCreate_게시물을_만들_수_있다() {
+    public void PostCreate로_게시물을_만들_수_있다() {
         // given
         PostCreate postCreate = PostCreate.builder()
                 .writerId(1)
@@ -38,6 +38,39 @@ class PostTest {
         assertThat(post.getWriter().getEmail()).isEqualTo("test@test.com");
         assertThat(post.getWriter().getNickname()).isEqualTo("tester");
         assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    public void PostUpdate로_게시물을_수정할_수_있다() {
+        // given
+        PostUpdate postUpdate = PostUpdate.builder()
+                .content("foobar")
+                .build();
+
+        User writer = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("tester")
+                .address("seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .content("helloworld")
+                .createdAt(1678530673958L)
+                .modifiedAt(0L)
+                .writer(writer)
+                .build();
+
+        // when
+        post = post.update(postUpdate, new TestClockHolder(1678530673958L));
+
+        // then
+        assertThat(post.getContent()).isEqualTo("foobar");
+        assertThat(post.getModifiedAt()).isEqualTo(1678530673958L);
+
     }
 
 }
