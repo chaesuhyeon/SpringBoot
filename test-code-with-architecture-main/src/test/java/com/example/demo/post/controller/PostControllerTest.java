76b9ac7da50b54e415818fc671200454aa928296
suhyeon chae <ddd8177@gmail.com>
2024-03-02 16:39:43 +0900

    PostController 소형테스트 진행

---
+++ test-code-with-architecture-main/src/test/java/com/example/demo/post/controller/PostControllerTest.java
@@ -0,0 +1,94 @@
+package com.example.demo.post.controller;
+
+import com.example.demo.mock.TestContainer;
+import com.example.demo.post.controller.response.PostResponse;
+import com.example.demo.post.domain.Post;
+import com.example.demo.post.domain.PostUpdate;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+public class PostControllerTest {
+
+    @Test
+    void 사용자는_게시물을_단건_조회_할_수_있다() throws Exception {
+        // given
+        TestContainer testContainer = TestContainer.builder().build();
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("test")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                .lastLoginAt(100L)
+                .build();
+        testContainer.userRepository.save(user);
+        testContainer.postRepository.save(Post.builder()
+                        .id(1L)
+                        .content("helloworld")
+                        .writer(user)
+                        .createdAt(100L)
+                .build());
+
+        // when
+        ResponseEntity<PostResponse> result = testContainer.postController.getPostById(1L);
+
+        // then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(result.getBody()).isNotNull();
+        assertThat(result.getBody().getContent()).isEqualTo("helloworld");
+        assertThat(result.getBody().getWriter().getNickname()).isEqualTo("test");
+        assertThat(result.getBody().getCreatedAt()).isEqualTo(100L);
+    }
+
+//    @Test
+//    void 사용자가_존재하지_않는_게시물을_조회할_경우_에러가_난다() throws Exception {
+//        // given
+//        // when
+//        // then
+//        mockMvc.perform(get("/api/posts/123456789"))
+//                .andExpect(status().isNotFound())
+//                .andExpect(content().string("Posts에서 ID 123456789를 찾을 수 없습니다."));
+//    }
+//
+//    @Test
+//    void 사용자는_게시물을_수정할_수_있다() throws Exception {
+//        // given
+//        PostUpdate postUpdate = PostUpdate.builder()
+//                .content("foobar")
+//                .build();
+//
+//        // when
+//        // then
+//        mockMvc.perform(
+//                        put("/api/posts/1")
+//                                .contentType(MediaType.APPLICATION_JSON)
+//                                .content(objectMapper.writeValueAsString(postUpdate)))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.id").isNumber())
+//                .andExpect(jsonPath("$.content").value("foobar"))
+//                .andExpect(jsonPath("$.writer.id").isNumber())
+//                .andExpect(jsonPath("$.writer.email").value("kok202@naver.com"))
+//                .andExpect(jsonPath("$.writer.nickname").value("kok202"));
+//    }
+}
